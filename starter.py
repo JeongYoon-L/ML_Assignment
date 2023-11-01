0e11f546f952396b69b607936a35e418dd9e6611
Rohan Gupta <rohangupta1704@gmail.com>
2023-10-31 21:05:40 -0500

    Refactored Jenny's PCA source code

--- starter.py
+++ starter.py
@@ -1,50 +1,26 @@
 from src.model.k_nearest_neighbor import KNearestNeighbor
 from src.model.kmeans import KMeans
 from src.utils.accuracy import get_accuracy
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
 
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 # returns a list of labels for the query dataset based upon labeled observations in the train dataset.
 # metric is a string specifying either "euclidean" or "cosim".  
 # All hyper-parameters should be hard-coded in the algorithm.
-def knn(train, validation, query, metric):
+def knn(train, query, metric, aggregator, k_value):
     features = train[0]
     labels = train[1]
-    K = 10
-    best_k = 0
-    best_acc = 0
-    n_components=60
 
-    pca = PCA(n_components)
-    reduced_features = pca.fit_transform(features)
-    reduced_validation = pca.transform(validation[:,1:785])
-    reduced_query = pca.transform(query)
+    query_features = query[0]
 
-    for k in range(1,K):
-        model = KNearestNeighbor(k, metric, "mode")
-        model.fit(reduced_features, labels)
+    model = KNearestNeighbor(k_value, metric, aggregator)
+    model.fit(features, labels)
 
-        predicted_labels = []
-        for v in reduced_validation:
-            predicted_labels.append(model.predict(v))
-        acc = get_accuracy(predicted_labels, validation[:, 0].tolist())
-        print("knn accuracy for K = %d , PCA = %d : %f" % (k,n_components, acc))
-        if acc > best_acc:
-            best_acc = acc
-            best_k = k
-
-    print(best_k)
-
-    best_model = KNearestNeighbor(best_k, metric, "mode")
-    best_model.fit(features, labels)
-
-
-    #For test data  
     predicted_labels = []
-    for q in reduced_query:
-        predicted_labels.append(best_model.predict(q))
+    for q in query_features:
+        predicted_labels.append(model.predict(q))
 
     return predicted_labels
 
@@ -92,22 +68,60 @@ def show(file_name, mode):
                 print(' ')
         print('LABEL: %s' % data_set[obs][0], end='')
         print(' ')
-            
+
+
+def get_best_k_value_for_knn(train, validation, metric, aggregator):
+    train_features = train[0]
+    train_labels = train[1]
+
+    validation_features = validation[0]
+    validation_labels = validation[1]
+
+    K = 10
+    best_k = 0
+    best_acc = 0
+    n_components = 60
+
+    pca = PCA(n_components)
+    reduced_train_features = pca.fit_transform(train_features)
+    reduced_validation_features = pca.transform(validation_features)
+
+    for k in range(1, K):
+        model = KNearestNeighbor(k, metric, aggregator)
+        model.fit(reduced_train_features, train_labels)
+
+        predicted_labels = []
+        for v in reduced_validation_features:
+            predicted_labels.append(model.predict(v))
+
+        acc = get_accuracy(predicted_labels, validation_labels.tolist())
+        print("knn test accuracy for K = %d , PCA = %d : %f" % (k, n_components, acc))
+
+        if acc > best_acc:
+            best_acc = acc
+            best_k = k
+
+    return best_k
+
+
 def main():
     # knn
     train_data = pd.read_csv('./src/data/train.csv').values
     test_data = pd.read_csv('./src/data/test.csv').values
-    valid_data = pd.read_csv('./src/data/valid.csv').values
+    validation_data = pd.read_csv('./src/data/valid.csv').values
 
     train = [train_data[:, 1:785], train_data[:, 0]]
-    query = test_data[:, 1:785]
-    predicted_labels = knn(train, valid_data, query, "euclidean")
+    test = [test_data[:, 1:785], test_data[:, 0]]
+    validation = [validation_data[:, 1:785], validation_data[:, 0]]
 
+    best_k_value = get_best_k_value_for_knn(train, validation, "euclidean", "mode")
+    best_k_value_predicted_labels = knn(train, test, "euclidean", "mode", best_k_value)
+    best_k_value_accuracy = get_accuracy(best_k_value_predicted_labels, test[1].tolist())
 
-    print("knn accuracy", get_accuracy(predicted_labels, test_data[:, 0].tolist()))
+    print("knn validation accuracy for K = %d : %f" % (best_k_value, best_k_value_accuracy))
 
     # kmeans
-    kmeans(train, query, "euclidean")
+    # kmeans(train, query, "euclidean")
 
 
 if __name__ == "__main__":
